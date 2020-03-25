46982be4f9d63ababc9678f3cc525d99d92b8fde
Marty <62232836+MartySalamea@users.noreply.github.com>
2020-03-25 08:23:20 -0500

    Add files via upload

---
+++ ProblemSet1/mario/less/mario.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void printHashes(length) {
+    for (int hashes = 0; hashes < length; hashes++) {
+        printf("#");
+    }
+}
+
+// Print left side spaces
+void printSpaces(length) {
+    for (int spaces = 0; spaces < length; spaces++) {
+        printf(" ");
+    }
+}
+
+int main(void)
+{
+    int size = -1;
+
+    // Request an appropriate size between 1 and 8
+    while (size < 1 || size > 8) {
+        size = get_int("Enter a size: ");
+    }
+
+    for(int i = 1; i <= size; i++) {
+
+        // Print left side spaces
+        printSpaces(size - i);
+
+        // Print left side hashes
+        printHashes(i);
+
+        // End of line
+        printf("\n");
+    }
+}
