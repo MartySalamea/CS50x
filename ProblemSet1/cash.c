8ca8dc3d20b478ede24d85b4d87575a1290459da
Marty <62232836+MartySalamea@users.noreply.github.com>
2020-03-24 22:00:39 -0500

    Add files via upload

--- ProblemSet1/cash.c
+++ ProblemSet1/cash.c
@@ -1,1 +1,51 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <math.h>
 
+int main(void)
+{
+    int intQuarter;
+    int intDime;
+    int intNickel;
+    int intPenny;
+    int changeOutput = 0;
+
+    float changeInput;
+    //Prompt user for change input
+    do
+    {
+        changeInput = get_float("Change owed: ");
+    }
+    while (changeInput < 0);
+    //Round to nearest cent
+    //Get cents from the dollar input
+    int cents = round(changeInput * 100);
+    //Get amount of quarters
+    for (intQuarter = 1; cents >= 25; intQuarter++)
+    {
+        cents -= 25;
+        changeOutput += 1;
+    }
+    //Get amount of dimes
+    for (intDime = 1; cents >= 10; intDime++)
+    {
+        cents -= 10;
+        changeOutput += 1;
+    }
+    //Get amount oof nickels
+    for (intNickel = 1; cents >= 5; intNickel++)
+    {
+        cents -= 5;
+        changeOutput += 1;
+    }
+    //Get amount of pennies
+    for (intPenny = 1; cents >= 1; intPenny++)
+    {
+        cents -= 1;
+        changeOutput += 1;
+    }
+    //Print output
+    printf("%i\n", changeOutput);
+
+
+}
